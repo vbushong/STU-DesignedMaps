92bfd340066c3dcced1a03fffbef6485a5885e7d
bushongv <Vanna.Bushong@espn.com>
2015-06-03 18:33:33 -0400

    Batch file for starting chrome for testing purposes

---
+++ chrome.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem Chrome with --allow-file-access-from-files
+cd C:\Program Files (x86)\Google\Chrome\Application
+start chrome.exe --allow-file-access-from-files C:\VCS\WPF\STU-DesignedMaps\maps.html
+
+exit
